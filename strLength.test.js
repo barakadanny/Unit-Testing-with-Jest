af9a8b69515d0d0cfbb5371c2407c6e33dbd3eae
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-08-16 18:03:51 +0300

    Delete unnecessary spaces

--- strLength.test.js
+++ strLength.test.js
@@ -2,13 +2,10 @@ const strL = require('./stringLength');
 
 // for example dance == 5;
 describe('Tests', () => {
-  it('string between 1 and 10', () => {
-    expect(strL('dancedancedance')).toThrow(
-      'String length must be between 1 and 10'
-    );
-  });
+  test('string between 1 and 10', () => {
+    const word = 'dance';
+    const wordLength = strL(word);
 
-  it('when word = dance the result is 5', () => {
-    expect(strL('dance')).toBe(5);
+    expect(wordLength).toBe(5);
   });
 });
--- stringLength.js
+++ stringLength.js
@@ -1,7 +1,7 @@
 const stringLength = (str) => {
   const word = str.length;
   if (word < 1 || word > 10) {
-    throw new Error('String length must be between 1 and 10');
+    throw 'String length must be between 1 and 10';
   } else {
     return word;
   }
