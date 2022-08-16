9f5f23456cacfab598a45a5a57edafe1abf48822
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-08-16 18:07:05 +0300

    Add test to check if the string is between 1 and 10

--- strLength.test.js
+++ strLength.test.js
@@ -2,10 +2,17 @@ const strL = require('./stringLength');
 
 // for example dance == 5;
 describe('Tests', () => {
-  test('string between 1 and 10', () => {
+  test('string dance == 5', () => {
     const word = 'dance';
     const wordLength = strL(word);
 
     expect(wordLength).toBe(5);
+  });
+
+  test('string dancedancedancedance == 20', () => {
+    const word = 'dancedancedancedance';
+    const wordLength = strL(word);
+
+    expect(wordLength).toBe(20);
   });
 });
